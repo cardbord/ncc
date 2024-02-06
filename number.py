98e13ba375e04847d7ef605019141e8c3e9e758b
cardbo <william.tweed@hotmail.com>
2024-02-06 08:49:11 +0000

    fixed number unscramble (still don't remember what it does)

--- number.py
+++ number.py
@@ -2,13 +2,15 @@ a = input("text>>>").replace(" ","")
 b = int(input("Enter len>>>"))
 a = list(a)
 nlist = {}
+
 for i in range(0,len(a),b):
     if not str(a[i])+str(a[i+1]) in nlist:
         nlist[str(a[i])+str(a[i+1])] = 0
+
 for xvar in nlist:
     for i in range(0,len(a),b):
-        if str(a[i])+str(a[i+1]) == i:
-            i+=1
+        if str(a[i])+str(a[i+1]) == xvar:
+            nlist[f"{xvar}"]+=1
 
 
 print(nlist)
