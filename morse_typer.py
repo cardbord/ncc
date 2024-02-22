462edfda819583926f9390d0bd0b4228729be8ac
cardbo <william.tweed@hotmail.com>
2024-02-22 17:48:44 +0000

    something

--- morse_typer.py
+++ morse_typer.py
@@ -1,14 +1,22 @@
-from morse import encode
+from morse import morse_alpha_dict
 from pynput import keyboard
 
 ctrl = keyboard.Controller()
+halt_spaces = False
 def repEngMorse(key:keyboard.KeyCode):
-    try:
-        ciphertext = encode(key.char)
+    
+    global halt_spaces
+    if isinstance(key,keyboard.KeyCode) and key.char.isalpha():
+        halt_spaces = True
         ctrl.tap(keyboard.Key.backspace)
-        ctrl.type(ciphertext)
-    except:
-        pass
+        ciphertext = morse_alpha_dict.get(key.char.lower())
+        ctrl.type(ciphertext+" ")
+        halt_spaces = False
+    if isinstance(key,keyboard.Key) and key == keyboard.Key.space:
+
+        if not halt_spaces:
+            ctrl.type("/")
+    
 
 with keyboard.Listener(on_press=repEngMorse) as listen:
     listen.join() 
