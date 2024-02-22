544fadfb37da5365e4cad3085e80e4a5bb3067e4
cardbo <william.tweed@hotmail.com>
2024-02-22 17:49:04 +0000

    added vigenere on-the-fly encryption

---
+++ vigenere_junk.py
@@ -0,0 +1,46 @@
+from vigenere import encrypt, decrypt
+from pynput import keyboard
+from pyperclip import paste
+from time import sleep
+v_key = input("ENter key>>>")
+
+
+ctrl = keyboard.Controller()
+halt_spaces = False
+def repEngVigenere(key:keyboard.Key):
+    if isinstance(key,keyboard.KeyCode) and key.char == "`":
+        ctrl.tap(keyboard.Key.backspace)
+        ctrl.tap(keyboard.Key.home)
+        ctrl.press(keyboard.Key.shift_l)
+        ctrl.tap(keyboard.Key.end)    
+        ctrl.release(keyboard.Key.shift_l)
+        ctrl.press(keyboard.Key.ctrl)
+        ctrl.tap("x")
+        ctrl.release(keyboard.Key.ctrl)
+        sleep(0.5)
+        text = paste()
+    
+        plaintext = decrypt(text.replace(" ",""),v_key)
+        ctrl.type(plaintext)
+    
+    elif key == keyboard.Key.delete:
+        ctrl.tap(keyboard.Key.home)
+        ctrl.press(keyboard.Key.shift_l)
+        ctrl.tap(keyboard.Key.end)    
+        ctrl.release(keyboard.Key.shift_l)
+        ctrl.press(keyboard.Key.ctrl)
+        ctrl.tap("x")
+        ctrl.release(keyboard.Key.ctrl)
+        sleep(0.5)
+        text = paste()
+        
+        ciphertext = encrypt(text.replace(" ",""),v_key)
+        ctrl.type(ciphertext)
+        
+        
+    
+
+with keyboard.Listener(on_press=repEngVigenere) as listen:
+    listen.join()
+    
+    
